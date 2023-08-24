712e50096cb3d7eb4a5450b3eb3b0674e9a1e271
aveldin <96331862+aveldin@users.noreply.github.com>
2023-08-24 23:13:58 +0700

    Add comments

--- update_storj.ps1
+++ update_storj.ps1
@@ -1,21 +1,18 @@
-# C:\Storj\nodes\Storj1\Storage Node
-# Copy-Item -Path "C:\SourceFolder\1.txt" -Destination "F:\DestFolder\1.txt"
+$count_nodes = 35   # Count nodes
+$serv = 'storj'     # Name of services
+$url = "https://github.com/storj/storj/releases/latest/download/storagenode_windows_amd64.zip"      # Address of the latest version
+$download = "C:\storagenode_windows_amd64.zip"      # What address to download
+$UnArchive = "C:\"                                  # To which address to unzip
+$UnArchive_file = $UnArchive + "storagenode.exe"    # Full address of the unzipped file
+$Address = "C:\Storj\nodes\Storj"                   # Address node
+$i = 1                                              # Counter
 
-$count_nodes = 35   # Количество нод
-$serv = 'storj'     # Название служб
-$url = "https://github.com/storj/storj/releases/latest/download/storagenode_windows_amd64.zip"      # Адрес последней версии
-$download = "C:\storagenode_windows_amd64.zip"      # По какому адресу скачать
-$UnArchive = "C:\"                                  # По какому адресу разархивировать
-$UnArchive_file = $UnArchive + "storagenode.exe"    # Полный адрес разархивированного файла
-$Address = "C:\Storj\nodes\Storj"                   # Адрес нод
-$i = 1                                              # Счётчик
-
-Invoke-WebRequest $url -OutFile $download           # Скачать файл
+Invoke-WebRequest $url -OutFile $download           # Download file 
 echo "Download Latest"
-Expand-Archive -Force $download $UnArchive          # Разархивировать файл
+Expand-Archive -Force $download $UnArchive          # Unzip file
 echo "Complete UnArchive"
 
-while ($i -ne $count_nodes) {                       # Останавливаем все ноды
+while ($i -ne $count_nodes) {                       # Stopping all nodes
     $serv_storj = $serv + $i.ToString()
     Set-Service $serv_storj -Status Stopped
     echo ("Storj " + $i + " Stopped")
@@ -23,18 +20,18 @@ while ($i -ne $count_nodes) {                       # Останавливаем
     sleep 1
 }
 
-$i = 1                                              # Сбрасываем счётчик
+$i = 1                                              # Resetting counter
 
-while ($i -ne $count_nodes) {                       # Обновляем исполнительные файлы
+while ($i -ne $count_nodes) {                       # Updating executive files
     $Address_full = $Address + $i.ToString() + "\Storage Node\"
     Copy-Item -Path $UnArchive_file -Destination $Address_full
     echo ("Storj " + $i + " update")
     $i += 1
 }
 
-$i = 1                                              # Сбрасываем счётчик
+$i = 1                                              # Resetting counter
 
-while ($i -ne $count_nodes) {                       # Запускаем все ноды
+while ($i -ne $count_nodes) {                       # Launching all nodes
     $serv_storj = $serv + $i.ToString()
     Set-Service $serv_storj -Status Running
     echo ("Storj " + $i + " Running")
